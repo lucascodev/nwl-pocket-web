b458726af9aec6ddbdf163debce6da94c34a6196
lucascodev <lucas.codev@hotmail.com>
2024-09-13 01:26:04 -0300

    Add UI components and dependencies
    Removed react.svg asset and added new components: RadioGroup, Input, and Separator. Updated package.json and package-lock.json to include new dependencies (Radix UI, Lucide React, Tailwind Merge, Tailwind Variants).

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,36 @@
-export function App() {
+import { Plus, X } from 'lucide-react'
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-illustration.svg'
+import { Button } from './components/ui/button'
+import { Dialog, DialogClose, DialogContent, DialogTitle, DialogTrigger } from './components/ui/dialog'
 
+export function App() {
   return (
-    <h1 className="bg-black text-white text-xl">Hello, World!</h1>
+    <Dialog>
+      <div className="h-screen flex flex-col items-center justify-center gap-8">
+        <img src={logo} alt="In Orbit logo" className="w-48" />
+        <img src={letsStart} alt="Let's start illustration" className="w-96" />
+        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
+          mesmo?
+        </p>
+
+        <DialogTrigger asChild>
+          <Button>
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <DialogContent>
+        <div className="flex items-center justify-between">
+          <DialogTitle>Cadastrar meta</DialogTitle>
+          <DialogClose>
+            <X className="size-5 text-zinc-600" />
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
