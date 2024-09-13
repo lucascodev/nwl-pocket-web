8bda3b79509e43220d76a39cf1933fb288297572
lucascodev <lucas.codev@hotmail.com>
2024-09-13 02:34:43 -0300

    **Add UI components for goal creation and empty state**
    Introduced `EmptyGoals` and `CreateGoal` components to handle empty state display and goal creation dialogs, improving user interaction when no goals are available. Also updated dependencies to include `@radix-ui/react-progress` for future progress tracking features.

--- src/app.tsx
+++ src/app.tsx
@@ -1,36 +1,16 @@
-import { Plus, X } from 'lucide-react'
-import logo from './assets/logo-in-orbit.svg'
-import letsStart from './assets/lets-start-illustration.svg'
-import { Button } from './components/ui/button'
-import { Dialog, DialogClose, DialogContent, DialogTitle, DialogTrigger } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+import {
+  Dialog
+} from './components/ui/dialog'
 
 export function App() {
   return (
     <Dialog>
-      <div className="h-screen flex flex-col items-center justify-center gap-8">
-        <img src={logo} alt="In Orbit logo" className="w-48" />
-        <img src={letsStart} alt="Let's start illustration" className="w-96" />
-        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
-          mesmo?
-        </p>
+      {/* <EmptyGoals /> */}
+      <Summary />
 
-        <DialogTrigger asChild>
-          <Button>
-            <Plus className="size-4" />
-            Cadastrar meta
-          </Button>
-        </DialogTrigger>
-      </div>
-
-      <DialogContent>
-        <div className="flex items-center justify-between">
-          <DialogTitle>Cadastrar meta</DialogTitle>
-          <DialogClose>
-            <X className="size-5 text-zinc-600" />
-          </DialogClose>
-        </div>
-      </DialogContent>
+      <CreateGoal />
     </Dialog>
   )
 }
